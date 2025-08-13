57a2e4ab669fa020ed7f2f617686d8471fd801bc
CaioDalNegro <caiodalnegro@gmail.com>
2025-08-12 22:32:39 -0300

    first commit

---
+++ src/main/java/br/com/itauchallenge/itau_transaction_challenge/ItauTransactionChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.itauchallenge.itau_transaction_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauTransactionChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauTransactionChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itauchallenge/itau_transaction_challenge/ItauTransactionChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itauchallenge.itau_transaction_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauTransactionChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
