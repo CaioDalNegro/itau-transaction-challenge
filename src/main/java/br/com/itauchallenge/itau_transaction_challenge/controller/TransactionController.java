9bc8fe774c109a4708276e260dab22d3b39fa801
CaioDalNegro <caiodalnegro@gmail.com>
2025-08-14 15:14:19 -0300

    criacao da lista de transaçoes

--- src/main/java/br/com/itauchallenge/itau_transaction_challenge/controller/TransactionController.java
+++ src/main/java/br/com/itauchallenge/itau_transaction_challenge/controller/TransactionController.java
@@ -1,20 +1,23 @@
 package br.com.itauchallenge.itau_transaction_challenge.controller;
 
+import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.itauchallenge.itau_transaction_challenge.model.TransactionModel;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@Controller
+//@Controller //@Controller é usado para retornar páginas HTML (views), não objetos JSON.
+@RestController
 @RequestMapping("/transacao")
 public class TransactionController {
     
@@ -26,16 +29,23 @@ public class TransactionController {
     @PostMapping
     public TransactionModel createTransaction(@RequestBody TransactionModel transaction) {
         transaction.setId(nextId++);                                             // Define ID único
+        transaction.setDataHora(OffsetDateTime.now());                           // Define a data e hora atual automaticamente
         transactions.put(transaction.getId(), transaction);                      // Guarda no mapa
         return transaction;                                                      // Retorna transação criada
     }
     
 
     // Apagar Transaçoes ------------------>
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}") 
     public String deleteTransaction(@PathVariable Long id){                                                
         if (transactions.remove(id) != null)                                     // Remove a transação do mapa usando o ID fornecido
             return "Transaçao removida!";                                        // Retorna mensagem de sucesso
         return "Transaçao nao foi removida!";                                    // Caso contrário, não havia transação com o ID informado
+    }
+
+    // Listar Transações------------------->
+    @GetMapping                                                                  // Mapeia requisições HTTP GET para /transacao
+    public Map<Long, TransactionModel> listarTransacoes() {                      
+        return transactions;                                                     // Retorna o mapa de transações
     }
 }
